a7dcf5de1cd9e528cd05d83bb4626448a72bfb0e
Kim Hyun <zrelor@gmail.com>
2023-02-28 01:32:17 +0900

    register UI 추가

--- pages/src/App.js
+++ pages/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
-import Register from "./page/register";
+import Register from "./page/register.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route path="/" exact  element={<Register />}></Route>
+        <Route path="/register" exact element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   );
